3befa03f676abf2102ca9a505041a31c8054e5db
PaulinoKanchi <31973902+PaulinoKanchi@users.noreply.github.com>
2018-01-31 20:34:53 -0600

    Add files via upload

---
+++ esquema_almacen.sql
@@ -0,0 +1,19 @@
+create table Almacen(Numero_Almacen integer,Ubicacion_Almacen varchar2(50),
+                      constraint PK_NUM_ALM PRIMARY KEY (Numero_Almacen));
+                      
+CREATE OR REPLACE PROCEDURE guardar_almacen(my_num_alm IN INTEGER,
+                                my_ubicacion IN varchar2)
+                                AS 
+                                BEGIN
+                                --CUERPO O LOGICA DEL PROCEDIMIENTO
+                      INSERT INTO almacen values(my_num_alm,my_ubicacion);
+                      end;
+                      /
+--usaremos un bloque PL SQL para probar que esta bien nuestro procedimiento
+
+BEGIN
+guardar_almacen(321,'ECATEPEC');
+END;
+/
+
+SELECT * FROM ALMACEN;
